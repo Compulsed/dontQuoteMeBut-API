9c1a0f539b902b91562f2cfec74b1141c7c8974c
Dale Salter <djsalter93@hotmail.com>
2014-08-01 02:15:22 +1000

    WORKING STATE: Makes static responses

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*	
+/*
  *	API route specifications and handler functions
  */
 
@@ -10,21 +10,11 @@ var express 		= require('express');
 var app 			= express();
 var http			= require('http').Server(app);
 var server 			= app.listen(port);
-var io 				= require('socket.io').listen(server);
 var cors			= require('cors');
 var bodyParser 		= require('body-parser');
-var mongoose 		= require('mongoose');
-
-var mongourl = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/api';
-mongoose.connect(mongourl);
-
-// Required Models
-var Chatroom		= require('./app/models/chatroom');
-var User 			= require('./app/models/user');
-var Message 		= require('./app/models/message');
 
 // Import request functionality
-var TrendRequest 	= require('./app/trend_request');
+var Middleware 	= require('./app/middleware');
 
 // Body parser is used to process data from a POST request
 app.use(bodyParser());
@@ -37,27 +27,6 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-//===================================
-//	SOCKET IO EVENT ROUTES
-//===================================
-var chat = io.of('/').on('connection', function(socket){
-	console.log("User Connected");
-
-	socket.on('message', function(msg) {
-		// Send to all but self
-		socket.join(msg.chatroom.chatroom);
-		console.log(msg);
-		socket.broadcast.to(msg.chatroom.chatroom).emit('message', msg);
-	});
-
-	socket.on('join', function(chatroom) {
-		socket.join(chatroom);
-	});
-
-	socket.on('error', function(error) {
-		console.log(error);
-	});
-});
 
 //===================================
 //	ROUTES
@@ -71,70 +40,13 @@ router.use(function(req, res, next){
 	next();
 });
 
-// Simple API route and response function
-router.get('/', function(req, res) {
-	res.sendfile('apiref.html');
-});
 
 // ---- /chatrooms ----
-router.route('/chatrooms')
-	.get(function(req, res) {
-		TrendRequest.twitterGlobal();
-		Chatroom.find({}, 'id chatroom source',function(err, trends) {
-			restRes = {'chatrooms': trends}
-			if (err)
-				res.send(err);
-
-			res.json(restRes);
-		});
+router.route('*')
+	.all(function(req, res, next) {
+		Middleware(req, res, next);
 	});
 
-// ---- /trends/location ----
-router.route('/chatrooms/location')
-	.get(function(req, res) {
-		var lat = req.query.lat;
-		var lon = req.query.lon;
-
-		TrendRequest.twitterLocation(lat, lon);
-		res.json({ message: "Check the console" });
-	});
-
-// ---- /users ----
-router.route('/users/:username')
-	.post(function(req, res) {
-		var user = new User();
-		user._id = req.params.username;
-		user.online = true;
-
-		user.save(function(err){
-			if (err)
-				res.send(err);
-
-			res.send("User Created");
-		});
-	});
-
-// ---- /messages/:trend ----
-router.route('/messages/:chatroom')
-	.post(function(req, res){
-		var message = new Message();
-		message.message = req.body.message;
-		message.chatroom = req.params.chatroom;
-		message.username = req.body.username;
-		
-		message.save(function(err){
-			if (err)
-				res.send(err);
-
-			res.send("Message Stored");
-		});
-	})
-
-	.get(function(req, res){
-		Message.find({chatroom: req.params.chatroom},"user message sent", function(err, messages){
-			res.json(messages);
-		});
-	});
 
 //===================================
 //	END ROUTES
